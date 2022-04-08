d1fd565d3d325c0bd8298510000c87073c1b5a45
Fabian-Boettges <95299029+Fabian-Boettges@users.noreply.github.com>
2022-04-08 14:30:32 +0200

    liked song component

--- spotify-react/src/App.js
+++ spotify-react/src/App.js
@@ -7,6 +7,7 @@ import Home from "./components/Home";
 import { Row } from "react-bootstrap";
 import Artist from "./components/Artist";
 import Album from "./components/Album";
+import LikedSongs from "./components/LikedSongs"
 
 let headers = new Headers({
   "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
@@ -55,6 +56,7 @@ class App extends React.Component {
             />
             <Route path="/artist/:id" component={Artist} />
             <Route path="/album/:id" component={Album} />
+            <Route path="/favorites" component = {<LikedSongs/>} />
           </Row>
         </div>
         <Player />
